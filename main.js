412ee98f27710c1e8ff725a8db000d52fb717d3e
Jose Carlos Urra <jose.urra86@gmail.com>
2020-10-25 12:13:36 +0100

    Cleaned for publishing

--- main.js
+++ main.js
@@ -7,13 +7,4 @@ module.exports = {
     buildSidebar
 }
 
-// const __dirname = path.dirname(new URL(import.meta.url).pathname).replace(/^\/|\/$/g, '')
-// console.log(__dirname)
-
-// let contents = buildContents(`${__dirname}/lib/__tests__/mocks/site/`, __dirname)
-// console.log(contents)
-// let sidebar = buildSidebar(contents)
-
-// console.log(sidebar)
-
 
