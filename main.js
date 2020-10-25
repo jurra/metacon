0566b2daa0ba1c52387c18517bdca6941c6a9e87
Jose Carlos Urra <jose.urra86@gmail.com>
2020-10-25 11:30:27 +0100

    Updated library

--- main.js
+++ main.js
@@ -3,15 +3,16 @@ import { buildContents } from "./lib/contentsBuilder.js"
 import { buildSidebar } from "./lib/adapters/vuepress.js"
 
 export {
-    buildContents
+    buildContents,
+    buildSidebar
 }
 
-const __dirname = path.dirname(new URL(import.meta.url).pathname).replace(/^\/|\/$/g, '')
-console.log(__dirname)
+// const __dirname = path.dirname(new URL(import.meta.url).pathname).replace(/^\/|\/$/g, '')
+// console.log(__dirname)
 
-let contents = buildContents(`${__dirname}/lib/__tests__/mocks/site/`, __dirname)
-let sidebar = buildSidebar(contents)
+// let contents = buildContents(`${__dirname}/lib/__tests__/mocks/site/`, __dirname)
+// let sidebar = buildSidebar(contents)
 
-console.log(sidebar)
+// console.log(sidebar)
 
 
